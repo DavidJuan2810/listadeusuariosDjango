88dc6a6ea2232a1ba0a878aa0bc5eb358f2f478d
DavidJuan2810 <Juandavides120@gmail.com>
2024-08-23 08:09:32 -0500

    Ultimos arreglos

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Persona (models.Model):
+     ESTADO_CHOICES=[
+        ('activo','Activo'),
+        ('inactivo','Inactivo')
+    ]
+     name= models.CharField(max_length=100)
+     cedula=models.CharField(max_length=100)
+     email=models.CharField(max_length=100)
+     dateCreate=models.DateField(auto_now=True)
+     estado=models.CharField(max_length=100, choices=ESTADO_CHOICES, default="activo")
+     
+     def __str__(self):
+       return self.name
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Persona
+from .form import PersonaForm
 
 # Create your views here.
+def registrar(request):
+    if request.method == 'POST':
+        form =PersonaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar')
+    form = PersonaForm()
+    return render (request,'registrarUsuario.html',{'form':form})
+
+def listar(request):
+    personas = Persona.objects.all()
+    return render(request,'listarUsuario.html',{'personas':personas})
+
+
